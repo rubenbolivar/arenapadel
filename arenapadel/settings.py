8f271136e4986015604f0d703b5f800c0240f685
rubenbolivar <rubenbe9@gmail.com>
2025-01-18 19:09:29 -0400

    Change static files storage to CompressedStaticFilesStorage

--- arenapadel/settings.py
+++ arenapadel/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
