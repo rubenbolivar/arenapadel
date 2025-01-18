9567d34f306ecfe48a03d065cd2b7a1a5133ac20
rubenbolivar <rubenbe9@gmail.com>
2025-01-18 19:14:00 -0400

    Change to Django's default StaticFilesStorage

--- arenapadel/settings.py
+++ arenapadel/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
