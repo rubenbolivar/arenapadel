0e330b5e37af6516ffae43ca2434afb0ca11cab0
rubenbolivar <rubenbe9@gmail.com>
2025-01-18 12:06:09 -0400

    feat: add dotenv configuration to load environment variables

--- arenapadel/settings.py
+++ arenapadel/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
