650546c77a10495170931fbe5830d0f80659dde4
rubenbolivar <rubenbe9@gmail.com>
2025-01-18 16:14:36 -0400

    fix: update DEBUG and ALLOWED_HOSTS for production

--- arenapadel/settings.py
+++ arenapadel/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['arenaspadel.club', 'www.arenaspadel.club', 'localhost', '127.0.0.1']
 
 # Application definition
 
