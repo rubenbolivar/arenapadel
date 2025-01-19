89084b0cf8d344bcf51076683900a5097c99b8a9
rubenbolivar <rubenbe9@gmail.com>
2025-01-18 22:05:23 -0400

    Update static files storage to ManifestStaticFilesStorage

--- arenapadel/settings.py
+++ arenapadel/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
